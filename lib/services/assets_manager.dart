2311d2752e79bc0156537e9388b0ad87a284479e
Amit Patil <amitgpatil215@gmail.com>
2023-03-28 02:29:10 +0530

    added logo

--- lib/services/assets_manager.dart
+++ lib/services/assets_manager.dart
@@ -3,4 +3,5 @@ class AssetsManager {
   static String userImage = "$imagePath/person.png";
   static String botImage = "$imagePath/chat_logo.png";
   static String openaiLogo = "$imagePath/openai_logo.jpg";
+  static String applogo = "$imagePath/logo.jpg";
 }
